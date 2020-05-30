7d8832a2e0773f6c4645cfa71ad6a43c756a1925
Mudassir Rehman <13276017+mudassir7446@users.noreply.github.com>
2020-05-31 02:34:10 +0530

    deploy aws

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiBaseUrl:"http://ec2-54-242-167-6.compute-1.amazonaws.com"
-  
+  apiBaseUrl:"http://ec2-54-242-167-6.compute-1.amazonaws.com",
+  context:'covidTrust'
 };
